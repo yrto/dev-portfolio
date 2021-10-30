4335710648502d4bdc5bdff3f7b853afa2e41100
Ayrton Mugnaini <yrto@outlook.com>
2021-10-29 21:02:55 -0300

    footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Layout from './__layout.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<footer>Copyright © 2021</footer>

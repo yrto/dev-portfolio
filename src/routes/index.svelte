ea26474452e317af00857480db619b4715025678
Ayrton Mugnaini <yrto@outlook.com>
2021-10-29 21:08:00 -0300

    header style update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
 	import Layout from './__layout.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<header>
+	<h1>Welcome to SvelteKit</h1>
+</header>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <footer>Copyright © 2021</footer>
+
+<style lang="postcss">
+	header {
+		@apply font-bold;
+	}
+</style>
